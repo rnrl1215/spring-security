947c228f3fee536560b827701694dcb693bff33d
skahn <d1215@naver.com>
2023-05-30 21:43:28 +0900

    feat: Add security configure

---
+++ src/main/java/com/security/example/config/ProjectConfig.java
@@ -0,0 +1,46 @@
+package com.security.example.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class ProjectConfig {
+    @Bean
+    public UserDetailsService userDetailsService() {
+        var userDetailsService
+                = new InMemoryUserDetailsManager();
+
+        UserDetails user = User.withUsername("skahn")
+                .password("12345")
+                .authorities("read")
+                .build();
+
+        userDetailsService.createUser(user);
+
+        return userDetailsService;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http.authorizeRequests()
+                .anyRequest()
+                .permitAll()
+                .and()
+                .build();
+    }
+
+}
